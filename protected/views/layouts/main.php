be0b140756001fcffa3628aa192429b6cae37e33
amaurylavieille <amaury.lavieille@gmail.com>
2014-01-07 11:49:23 +0100

    Ajout fichier config

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -9,14 +9,28 @@
 	</head>
 	<body>
 		<header id="header" class="line" role="banner">
-			<nav id="navigation" role="navigation">
+			<h1 class="left"><a href="<?php echo Yii::app()->request->baseUrl; ?>">libebook</a></h1>
+			<nav id="navigation" class="right" role="navigation">
 				<?php $this->widget('zii.widgets.CMenu',array(
 				'items'=>array(
-					array('label'=>'Home', 'url'=>array('/site/index'))
+					array('label'=>'catalogues', 'url'=>array('/site/index' )),
+					array('label'=>'aide', 'url'=>array('/site/index' )),
 					),
 				)); ?>
+
+				<?php   
+				echo CHtml::beginForm(array('search/search'), 'get', array('style'=> 'inline'));
+
+        		echo CHtml::textField('q', '', array('placeholder'=> 'rechercher...','style'=>'width:140px;'));
+
+        		echo CHtml::submitButton('Go!',array('style'=>'width:30px;'));
+        		echo CHtml::endForm('');
+
+        		?>
 			</nav>
+
 		</header>
+
 		<div id="main" role="main" class="line">
 			<?php echo $content; ?>
 
