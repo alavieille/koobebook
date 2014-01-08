735f73d80f79a9e5fae048e1959881c27a61ff55
amaurylavieille <amaury.lavieille@gmail.com>
2014-01-08 21:46:42 +0100

    Squelette header + style

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,33 +5,27 @@
 			<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/knacss.css" media="all">
-			<!-- <link rel="stylesheet" href="css/styles.css" media="all"> -->
+			<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" media="all"> 
 	</head>
 	<body>
-		<header id="header" class="line" role="banner">
-			<h1 class="left"><a href="<?php echo Yii::app()->request->baseUrl; ?>">libebook</a></h1>
-			<nav id="navigation" class="right" role="navigation">
-				<?php $this->widget('zii.widgets.CMenu',array(
-				'items'=>array(
-					array('label'=>'catalogues', 'url'=>array('/site/index' )),
-					array('label'=>'aide', 'url'=>array('/site/index' )),
-					),
-				)); ?>
+		<header id="header" class="line pa1" role="banner">
+			<div class="center">
+				<h1 class="left ml1" id="logo" ><a href="<?php echo Yii::app()->request->baseUrl; ?>" class="logo inbl">Lib<span>ebook</span></a></h1>
+				<nav id="navigation" class="right mr3 bigger" role="navigation">
+					<?php $this->widget('zii.widgets.CMenu',array(
+					'items'=>array(
+						array('label'=>'Catalogues', 'url'=>array('/site/index' )),
+						array('label'=>'Aide', 'url'=>array('/site/index' )),
+						),
+					'itemCssClass' => "inbl pr2",
 
-				<?php   
-				echo CHtml::beginForm(array('search/search'), 'get', array('style'=> 'inline'));
-
-        		echo CHtml::textField('q', '', array('placeholder'=> 'rechercher...','style'=>'width:140px;'));
-
-        		echo CHtml::submitButton('Go!',array('style'=>'width:30px;'));
-        		echo CHtml::endForm('');
-
-        		?>
-			</nav>
+					)); ?>
+				</nav>
+			</div>
 
 		</header>
 
-		<div id="main" role="main" class="line">
+		<div id="main" role="main" class="line center">
 			<?php echo $content; ?>
 
 		</div>
