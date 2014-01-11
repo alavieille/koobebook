5794dd6304efea513d2c315aba16396d8c8b49bb
amaurylavieille <amaury.lavieille@gmail.com>
2014-01-11 15:14:37 +0100

    Ajout header smartphone/tablet

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -8,19 +8,30 @@
 			<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" media="all"> 
 	</head>
 	<body>
-		<header id="header" class="line pa1" role="banner">
+		<header id="header" class="line pa1 " role="banner">
 			<div class="center">
-				<h1 class="left ml1" id="logo" ><a href="<?php echo Yii::app()->request->baseUrl; ?>" class="logo inbl">Lib<span>ebook</span></a></h1>
-				<nav id="navigation" class="right mr3 bigger" role="navigation">
+				<h1 class="left ml1 txtcenter small-w100" id="logo" ><a href="<?php echo Yii::app()->request->baseUrl; ?>" class="logo inbl">Lib<span>ebook</span></a></h1>
+				
+				<!-- ========== compact navigation =========== !-->
+				<nav id="compactNav" class="grid desktop-hidden small-w100 mt2">
+					<ul class="grid2">
+						<li id="showMenu" class=" inbl small-w50 ">menu</li>
+						<li id="showSearch" class="inbl small-w50">recherche</li>
+					</ul>
+				</nav>
+		
+
+				<nav id="navigation" class="right mr3 bigger small-w100 txtcenter " role="navigation">
 					<?php $this->widget('zii.widgets.CMenu',array(
 					'items'=>array(
 						array('label'=>'Catalogues', 'url'=>array('/site/index' )),
 						array('label'=>'Aide', 'url'=>array('/site/index' )),
 						),
-					'itemCssClass' => "inbl pr2",
+					'itemCssClass' => "inbl pr2 small-w100",
 
 					)); ?>
 				</nav>
+
 			</div>
 
 		</header>
