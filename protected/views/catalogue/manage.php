59ded98f6fd3e87416a115304734f96cc5482e52
amaurylavieille <amaury.lavieille@gmail.com>
2014-02-16 17:47:50 +0100

    Mise a jour knacss + correctif css

--- protected/views/catalogue/manage.php
+++ protected/views/catalogue/manage.php
@@ -6,11 +6,11 @@ $this->pageTitle=Yii::app()->name . ' - Mise a jour';
 ?>
 
 <h2 class="pt2 pb1 txtcenter">Gérer</h2>
-<section id="ltBook" class="knacss-debug w100">
+<section id="ltBook" class="knacss-debug w100 autogrid3">
 <h4>Vos livres</h4>
 <?php foreach ($model->books as $book) :?>
 
-	<div class="w150p knacss-debug inbl">
+	<div class="">
 		<a href="">
 		<?php 
  		$picture = yii::app()->baseUrl.DIRECTORY_SEPARATOR.'images/default_cover.png';
@@ -19,10 +19,18 @@ $this->pageTitle=Yii::app()->name . ' - Mise a jour';
  			$picture = $urlUpload.DIRECTORY_SEPARATOR.$book->id."-".$book->picture;
  		}
 		?>
-		<img class="w75" src="<?php echo $picture; ?>" alt="couverture">
+		<img class="w100p" src="<?php echo $picture; ?>" alt="couverture">
 		<p class="w100 pl1 pr1"><?php echo $book->title;?></p>		
 		</a>
 	</div>
 <?php endforeach; ?>
 </section>
 
+<div class="autogrid3">  <!-- 3 columns  -->
+  <div>lorem ipsum</div>
+  <div>lorem ipsum</div>
+  <div>lorem ipsum</div>
+  <div>lorem ipsum</div>
+  <div>lorem ipsum</div>
+  <div>lorem ipsum</div>
+</div>
