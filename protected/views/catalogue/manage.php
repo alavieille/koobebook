f6d36420271e89dbf7f015cbcf0c3a6d67deaf98
amaurylavieille <amaury.lavieille@gmail.com>
2014-02-17 20:36:44 +0100

    Ajout de livre depuis la gestion du catalogue

--- protected/views/catalogue/manage.php
+++ protected/views/catalogue/manage.php
@@ -7,8 +7,14 @@ $this->pageTitle=Yii::app()->name . ' - Mise a jour';
 
 <h2 class="pt2 pb1 txtcenter">Gérer</h2>
 <section id="ltBook" class="w100 pa1">
-<h4 class="pb1 mb1 w100">Vos livres</h4>
+<h4 class="pb1 mb1 w100">Vos ebooks</h4>
 <div class="autogrid4">
+	<div class="center ml1 mb2 mr1 mt2 w150p small-w100" id="addEbook">
+		<a class="center" href="<?php echo Yii::app()->createUrl('book/create/'); ?>">
+			<p id="iconAdd" class="txtcenter">+</p>
+			<p class="txtcenter ma0">Ajouter un livre</p>
+		</a>
+	</div>
 <?php foreach ($model->books as $book) :?>
 
 	<div class="mb2 mr1">
@@ -20,7 +26,7 @@ $this->pageTitle=Yii::app()->name . ' - Mise a jour';
  			$picture = $urlUpload.DIRECTORY_SEPARATOR.$book->id."-".$book->picture;
  		}
 		?>
-		<figure class="txtcenter">
+		<figure class="txtcenter pt1">
 			<img class="" src="<?php echo $picture; ?>" alt="couverture">
 			<figcaption>
 				<p class="pt1 w100 txtcenter"><?php echo $book->title;?></p>		
